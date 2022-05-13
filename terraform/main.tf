c91b4e079710c61b103e33c13cf751f7fa9fa823
Tor Ivar <torivara@gmail.com>
2022-05-13 11:18:48 +0200

    Update

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,7 +35,7 @@ resource "azurerm_subnet" "subnet" {
   address_prefix       = "10.0.0.0/25"
 }
 # Create Subnet
-resource "azurerm_subnet" "subnet" {
+resource "azurerm_subnet" "subnet2" {
   name                 = "subnet2"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
