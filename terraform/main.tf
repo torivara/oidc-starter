0b45cd5e5ba97ccc5028bcc7b5a1c7db3b920bfd
Tor Ivar <torivara@gmail.com>
2022-05-13 11:09:19 +0200

    Added terraform

---
+++ terraform/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tia-testingterraform2313-rg"
+    storage_account_name = "testingterraform2313"
+    container_name       = "terraformstate"
+    key                  = "terraformstate.tfstate"
+  }
+}
+
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
+}
+
+data "azurerm_client_config" "current" {}
+
+#Create Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "tf-demo-rg"
+  location = "westeurope"
+}
+
+#Create Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "tf-demo-vnet"
+  address_space       = ["10.0.0.0/24"]
+  location            = "westeurope"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "10.0.0.0/25"
+}
